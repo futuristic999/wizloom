006c85005a9b3caba0358905e6ed314123e73ae2
futuristic999 <futuristic999@gmail.com>
2012-01-06 22:15:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wizloom::Application.config.secret_token = 'c399974dbc5ce43cd44e086d5919163cc415e04be934aa5423050aa96e6289fddd7704e0141b507f00612f2978fbf458c68dd037bc6b3b830b7e2ef36bad22a8'
