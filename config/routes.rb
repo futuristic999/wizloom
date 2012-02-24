9036841fecf8a1c6b0d68f40c8d3d9afca03c805
futuristic999 <futuristic999@gmail.com>
2012-02-24 15:36:34 +0000

    Added Templates and Entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,43 @@
 Wizloom::Application.routes.draw do
+
+  get "entries/add"
+
+  get "entries/show"
+
+  get "templated_entries/add"
+
+  get "templated_entries/show"
+
+  get "templated_entries/delete"
+
+  get "templated_entries/edit"
+
+
+  match "entries"           => "entries#index"
+  match "entries/save"      => "entries#save"
+  match "entries/get/:id"   => "entries#get"
+  match "entries/list"      => "entries#list"
+  match "entries/delete/:id"    => "entries#delete"
+
+  match  "templates.json/get/:id" => "templates#get"
+  match  "templates/get/:id"    => "templates#get"
+  match  "templates/delete/:id" => "templates#delete"
+  match  "templates/edit/:id"   => "templates#edit" 
+  match  "templates/list"   => "templates#list"
+  match  "templates/add"    => "templates#add"
+  match  "templates/save"   => "templates#save"
+  match  "templates/:name"  => "templates#show"
+  match  "templates"        => "templates#index"
+
+
+
   get "pages/home"
 
   get "pages/contact"
   
   get "pages/about"
+
+  match "/" => "pages#home"
 
   resources :weight_entries
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,60 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120106233603) do
+ActiveRecord::Schema.define(:version => 20120223153329) do
+
+  create_table "entries", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "creator"
+    t.string   "status"
+    t.boolean  "refresh_html"
+    t.integer  "fieldvalues_id"
+    t.integer  "template_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "entries", ["fieldvalues_id"], :name => "index_entries_on_fieldvalues_id"
+  add_index "entries", ["template_id"], :name => "index_entries_on_template_id"
+
+  create_table "field_values", :force => true do |t|
+    t.integer  "entry_id"
+    t.string   "label"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "field_values", ["entry_id"], :name => "index_field_values_on_entry_id"
+
+  create_table "fields", :force => true do |t|
+    t.string   "label"
+    t.string   "options"
+    t.string   "default"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "template_id"
+    t.string   "style"
+    t.integer  "display_order"
+    t.string   "fieldtype"
+    t.string   "fieldname"
+  end
+
+  create_table "templates", :force => true do |t|
+    t.string   "name"
+    t.integer  "creator"
+    t.text     "xml"
+    t.text     "theme"
+    t.boolean  "refresh_html"
+    t.string   "status"
+    t.time     "created"
+    t.time     "modified"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "stub"
+    t.text     "cached_html"
+  end
 
   create_table "weight_entries", :force => true do |t|
     t.integer  "weight"
